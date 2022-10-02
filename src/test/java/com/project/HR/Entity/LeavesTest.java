9a74ab90f1b514453d2c1284b6835ceb7917b849
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-10-02 13:24:32 +0200

    test leaves

---
+++ src/test/java/com/project/HR/Entity/LeavesTest.java
@@ -0,0 +1,9 @@
+package com.project.HR.Entity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LeavesTest {
+    @Test
+    void builder() {
+    }
+}
