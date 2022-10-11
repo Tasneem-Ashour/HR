eddd0d7f81a17e6c068ce09f1d6cd5015137733e
Tasneem-Ashour <103181930+Tasneem-Ashour@users.noreply.github.com>
2022-10-11 11:51:57 +0200

    show token as json in body

--- src/main/java/com/project/HR/security/Filter/CustomAuthenticationFilter.java
+++ src/main/java/com/project/HR/security/Filter/CustomAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.project.HR.security.Filter;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,10 +17,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-private  final AuthenticationManager authenticationManager;
+    private static final String APPLICATION_JSON_VALUE = "application/json";
+    private  final AuthenticationManager authenticationManager;
 public  CustomAuthenticationFilter(AuthenticationManager authenticationManager){
     this.authenticationManager=authenticationManager;
 }
@@ -49,8 +53,14 @@ public  CustomAuthenticationFilter(AuthenticationManager authenticationManager){
                 .withIssuer(request.getRequestURI().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_taken",access_taken);
-        response.setHeader("refresh_token",refresh_token);
+//        response.setHeader("access_taken",access_taken);
+//        response.setHeader("refresh_token",refresh_token);
+        Map<String,String> tokens  = new HashMap<>();
+        tokens.put("access_taken",access_taken);
+        tokens.put("refresh_token",refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(),tokens);
+
 
 
     }
